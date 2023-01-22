8104f954580494393b4db83d415dd0f08604a128
Gustavo Ferri Alixandre <gustavo.alixandre@luizalabs.com>
2023-01-21 22:36:30 -0400

    Add Pause text

--- src/score.js
+++ src/score.js
@@ -12,12 +12,26 @@ export default class Score {
     this.textLifes = new PIXI.Text(`Vidas: ${this.player.lifes}`, textColor);
     this.textLifes.position.set(0, 30);
 
+    this.textLifes = new PIXI.Text(`Pausado`, textColor);
+    this.textLifes.position.set(0, 30);
+
     const middleWidth = app.screen.width / 2;
     const middleHeight = app.screen.height / 6;
     this.textEnd = new PIXI.Text("Game Over", textColor);
     this.textEnd.visible = false;
     this.textEnd.position.set(middleWidth, middleHeight);
     this.textEnd.anchor.set(0.5);
+
+    this.textPaused = new PIXI.Text(`Pausado`, textColor);
+    this.textPaused.position.set(middleWidth, middleHeight);
+    this.textPaused.anchor.set(0.5);
+    this.textPaused.visible = false;
+
+    this.app.stage.addChild(this.textPaused);
+  }
+
+  set showPaused(paused) {
+    this.textPaused.visible = paused;
   }
 
   update() {
