46d928f2dd9c958cd8292155a83608e410092eb9
Gustavo Ferri Alixandre <gustavo.alixandre@luizalabs.com>
2023-01-23 19:54:57 -0400

    Create timeout & interval functions

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 import Menu from "../src/menu";
+import { timeout, interval } from "./lib/pixi-timeout.js";
 
 const canvas = document.getElementById("mycanvas");
 const app = new PIXI.Application({
@@ -6,5 +7,7 @@ const app = new PIXI.Application({
   width: window.innerWidth,
   height: window.innerHeight,
 });
+timeout(app);
+interval(app);
 
 new Menu({ app });
---
+++ public/lib/pixi-timeout.js
@@ -0,0 +1,49 @@
+function timeout(pixi) {
+  function setTimeout(secs, cb) {
+    var progress = 0;
+
+    var ticker = function ticker(delta) {
+      progress += delta;
+      var elapsed = progress / (60 * pixi.ticker.speed);
+      if (elapsed > secs) end(true);
+    };
+
+    var end = function end(fire) {
+      pixi.ticker.remove(ticker);
+      if (fire) cb();
+    };
+
+    var clear = function clear() {
+      end(false);
+    };
+
+    var finish = function finish() {
+      end(true);
+    };
+
+    pixi.ticker.add(ticker);
+    return {
+      clear: clear,
+      finish: finish,
+    };
+  }
+
+  function clearTimeout(timerObj) {
+    timerObj.clear();
+  }
+
+  pixi.setTimeout = setTimeout;
+  pixi.clearTimeout = clearTimeout;
+}
+
+function interval(app) {
+  app.setInterval = (cb, time) => {
+    const timer = app.setTimeout(time, () => {
+      cb();
+      timer.clear();
+      app.setInterval(cb, time);
+    });
+  };
+}
+
+export { interval, timeout };
